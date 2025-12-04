cdc5b997e50a58b1d000e4d1ad37324a40a0d37f
IlgarMac <=>
2025-12-04 20:20:44 +0300

    change dvc config keys

--- sync_service/entrypoint.sh
+++ sync_service/entrypoint.sh
@@ -11,13 +11,13 @@ git checkout -b "$DVC_SYNC_BRANCH"
 git pull origin "$DVC_SYNC_BRANCH" || true
 dvc pull || true
 
-dvc remote remove models || true
-dvc remote add models s3://"$DVC_SYNC_S3_BUCKET"
-dvc remote modify models endpointurl "$DVC_ENDPOINT_URL"
-dvc remote modify models access_key_id "$AWS_ACCESS_KEY_ID"
-dvc remote modify models secret_access_key "$AWS_SECRET_ACCESS_KEY"
-dvc remote modify models region "$S3_REGION"
-dvc remote modify models use_ssl false
+# dvc remote remove models || true
+# dvc remote add models s3://"$DVC_SYNC_S3_BUCKET"
+# dvc remote modify models endpointurl "$DVC_ENDPOINT_URL"
+# dvc remote modify models access_key_id "$AWS_ACCESS_KEY_ID"
+# dvc remote modify models secret_access_key "$AWS_SECRET_ACCESS_KEY"
+# dvc remote modify models region "$S3_REGION"
+# dvc remote modify models use_ssl false
 
 git config --global user.email "$AUTO_MAIL"
 git config --global user.name "$AUTO_NAME"
